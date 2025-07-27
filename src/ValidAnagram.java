bf56ed0ca44deaa01447d3e4bb9db32c1c2c1b78
lalit0024 <lalit.nitr@gmail.com>
2025-07-27 00:45:12 -0400

    Update ValidAnagram.java
    Anagram solved

--- src/ValidAnagram.java
+++ src/ValidAnagram.java
@@ -1,5 +1,22 @@
+import java.util.Arrays;
+
 public class ValidAnagram {
     public static void main(String[] args) {
-
+        String s = "anagram", t = "nagaram" ;
+        if(s.length() != t.length()) {
+            System.out.println("false");
+            return;
+        }
+        char[] codes = new char[256];
+        for (int i = 0; i < s.length(); i++) {
+            codes[s.charAt(i)]++;
+            codes[t.charAt(i)]--;
+        }
+        Arrays.sort(codes);
+        if(codes[codes.length-1]==0){
+            System.out.println("true");
+        }else  {
+            System.out.println("false");
+        }
     }
 }
